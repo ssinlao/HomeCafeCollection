ae1cffd2167899cda9be29717936bac52963b4cd
mmisaacs <miss.maddie.isaacs@gmail.com>
2025-04-25 10:09:38 -0700

    set up react files

---
+++ homeCafeCollection/frontend/src/search.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function App() {
+    return (
+        <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+            <h1>Welcome to Home Cafe Collection</h1>
+            <p>Gluten-Free Orange Chicken Recipe coming soon!</p>
+        </div>
+    );
+}
